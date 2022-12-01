42c5ccc7e319331ca298a84102e77c6f469de31d
Gergana Georgieva <gerganagergana1@gmail.com>
2022-12-01 18:34:16 +0200

    C# OOP Exam - 09 April 2022

--- SmartphoneShop.Tests/SmartphoneShopTests.cs
+++ SmartphoneShop.Tests/SmartphoneShopTests.cs
@@ -5,6 +5,20 @@ namespace SmartphoneShop.Tests
     [TestFixture]
     public class SmartphoneShopTests
     {
-       
+        private Smartphone smartphone;
+        private Shop shop;
+
+       [SetUp]
+       public void SetUp()
+        {
+            this.smartphone = new Smartphone("phoneModel", 100);
+            this.shop = new Shop(5);
+        }
+
+        [Test]
+        public void ShopCapacityGetterWorksCorrectly()
+        {
+            Assert.That(shop.Capacity, Is.EqualTo(5));
+        }
     }
 }
